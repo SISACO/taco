f5f186b7e8de573b64c31d64847b009d2776fd76
SISACO <athulrammp3@gmail.com>
2024-04-19 15:39:33 +0530

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:Taco/screens/HomePage.dart';
 import 'package:Taco/screens/SignIN.dart';
 import 'package:Taco/screens/SignUP.dart';
 import 'package:Taco/screens/resetPass.dart';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -15,8 +19,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      title: 'Flutter Demoo',
+      debugShowCheckedModeBanner: false,
+      title: 'Taco',
       theme: ThemeData(
         useMaterial3: true,
       ),
@@ -30,3 +36,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
